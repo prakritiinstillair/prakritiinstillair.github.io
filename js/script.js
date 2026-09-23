8fa400493ceac6383907f25e8624f1813764f674
prakritiinstillair <prakritiinstillair@gmail.com>
2026-09-23 10:26:18 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,18 +44,18 @@ if (menuIcon && closeBtn && menu) {
     const top1 = rand(5, 70);
     const height1 = rand(8, 25);
     const btm1 = 100 - (top1 + height1);
-    const shiftX1 = rand(-3, 3); // 帯が横にズレることで、そのエリアの文字が「ズレて見える」
+    const shiftX1 = rand(-1, 1); // 帯が横にズレることで、そのエリアの文字が「ズレて見える」
     const skew1 = rand(-25, 25);
 
     // 帯2のカット位置とスライド量
     const top2 = rand(10, 80);
     const height2 = rand(5, 30);
     const btm2 = 100 - (top2 + height2);
-    const shiftX2 = rand(-4, 4);
+    const shiftX2 = rand(-1, 1);
     const skew2 = rand(-20, 20);
 
     // 色反転と色の回転
-    const hue = rand(-60, 60);
+    const hue = rand(-20, 20);
     const tearOpacity = (Math.random() * 0.12 + 0.04).toFixed(2);
 
     // ※ bodyShift 系の更新は削除
